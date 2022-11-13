d4e1d76633b1849ca2475d0d8afcc3b08a85d99d
truc2203 <tranthanhtruc22031998@gmail.com>
2022-11-13 10:36:31 +0700

    add item

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { MdFastfood } from "react-icons/md";
+import {
+  MdAttachMoney,
+  MdCloudUpload,
+  MdDelete,
+  MdFastfood,
+  MdFoodBank,
+} from "react-icons/md";
 import { categories } from "../utils/data";
 import Loader from "./Loader";
 const CreateContainer = () => {
@@ -13,6 +19,10 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const upLoadImage = () => {};
+  const deleteImage = () => {};
+  const saveDetails = () => {};
 
   return (
     <>
@@ -67,7 +77,76 @@ const CreateContainer = () => {
             </select>
           </div>
           <div className="w-full group flex items-center justify-center flex-col border-2 border-dotted border-gray-300 h-225 md:h-420 cursor-pointer rounded-lg">
-            {isLoading ? <Loader /> : <></>}
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <>
+                {!imageAsset ? (
+                  <>
+                    <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                      <div className="w-full h-full flex flex-col items-center justify-center cursor-pointer gap-2">
+                        <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                        <p className="text-gray-500 hover:text-gray-700">
+                          Click Here Yp Up Load
+                        </p>
+                      </div>
+                      <input
+                        type="file"
+                        name="uploadimage"
+                        accept="image/*"
+                        onChange={upLoadImage}
+                        className="w-0 h-0"
+                      />
+                    </label>
+                  </>
+                ) : (
+                  <>
+                    <div className="relative h-full">
+                      <img
+                        src={imageAsset}
+                        alt="upload img"
+                        className="w-full h-full object-cover"
+                      />
+                      <button
+                        type="button"
+                        className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
+                        onClick={deleteImage}
+                      >
+                        <MdDelete className="text-white" />
+                      </button>
+                    </div>
+                  </>
+                )}
+              </>
+            )}
+          </div>
+
+          <div className="w-full flex flex-col md:flex-row items-center gap-3 ">
+            <div className="w-full py-2 border-b border-gray-200 flex item-center gap-2">
+              <MdFoodBank className="text-gray-700 text-2xl" />
+              <input
+                type="text"
+                required
+                value={calories}
+                onChange={(e) => setCalories(e.target.value)}
+                placeholder="Calories"
+                className="w-full h-full text-lg bg-transparent outline-none border-none placehoder:text-gray-700"
+              />
+            </div>
+            <div className="w-full py-2 border-b border-gray-200 flex item-center gap-2">
+              <MdAttachMoney className="text-gray-700 text-2xl" />
+              <input
+                type="text"
+                required
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="price"
+                className="w-full h-full text-lg bg-transparent outline-none border-none placehoder:text-gray-700"
+              />
+            </div>
+          </div>
+          <div className="flex item-center w-full">
+            <button type="button" className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-slate-200 font-bold" onClick={saveDetails}>Save </button>
           </div>
         </div>
       </div>
